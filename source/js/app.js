ec59004e935765034ad0adad4c01797ccb08a769
Дмитрий <buglak.dimitri@yandex.ru>
2019-12-04 11:33:56 +0300

    Для github pages

--- source/js/app.js
+++
@@ -1,4 +0,0 @@
-const menuButton = document.querySelector('.site-navigation__toggler');
-menuButton.addEventListener('click', function () {
-  menuButton.classList.toggle('site-navigation__toggler--close');
-});
